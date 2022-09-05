568c285b5b2741b3ac092bec48953918eba4bccf
annibuliful <lagmanmc@gmail.com>
2022-09-05 17:51:21 +0700

    chore: encode-worker.js

--- src/js/encode-worker.js
+++ src/js/encode-worker.js
@@ -1,19 +1,24 @@
-importScripts('./webm-writer2.js')
+importScripts('./webm-writer2.js');
 
 let webmWriter = null;
 let fileWritableStream = null;
 let frameReader = null;
 
-async function startRecording(fileHandle, frameStream, trackSettings) {
+async function startRecording(
+  fileHandle,
+  frameStream,
+  trackSettings
+) {
   let frameCounter = 0;
 
   fileWritableStream = await fileHandle.createWritable();
 
   webmWriter = new WebMWriter({
-      fileWriter: fileWritableStream,
-      codec: 'VP9',
-      width: trackSettings.width,
-      height: trackSettings.height});
+    fileWriter: fileWritableStream,
+    codec: 'VP9',
+    width: trackSettings.width,
+    height: trackSettings.height,
+  });
 
   frameReader = frameStream.getReader();
 
@@ -24,11 +29,11 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
     error: (e) => {
       console.log(e.message);
       stopRecording();
-    }
+    },
   };
 
   const config = {
-    codec: "vp09.00.10.08",
+    codec: 'vp09.00.10.08',
     width: trackSettings.width,
     height: trackSettings.height,
     bitrate: 10e6,
@@ -39,29 +44,31 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
   console.assert(support.supported);
   encoder.configure(config);
 
-  frameReader.read().then(async function processFrame({done, value}) {
-    let frame = value;
+  frameReader
+    .read()
+    .then(async function processFrame({ done, value }) {
+      let frame = value;
 
-    if(done) {
-      await encoder.flush();
-      encoder.close();
-      return;
-    }
-
-    if (encoder.encodeQueueSize <= 30) {
-      if (++frameCounter % 20 == 0) {
-        console.log(frameCounter + ' frames processed');
+      if (done) {
+        await encoder.flush();
+        encoder.close();
+        return;
       }
 
-      const insert_keyframe = (frameCounter % 150) == 0;
-      encoder.encode(frame, { keyFrame: insert_keyframe });
-    } else {
-      console.log('dropping frame, encoder falling behind');
-    }
+      if (encoder.encodeQueueSize <= 30) {
+        if (++frameCounter % 20 == 0) {
+          console.log(frameCounter + ' frames processed');
+        }
 
-    frame.close();
-    frameReader.read().then(processFrame);
-  });
+        const insert_keyframe = frameCounter % 150 == 0;
+        encoder.encode(frame, { keyFrame: insert_keyframe });
+      } else {
+        console.log('dropping frame, encoder falling behind');
+      }
+
+      frame.close();
+      frameReader.read().then(processFrame);
+    });
 }
 
 async function stopRecording() {
@@ -73,13 +80,16 @@ async function stopRecording() {
   fileWritableStream = null;
 }
 
-self.addEventListener('message', function(e) {
+self.addEventListener('message', function (e) {
   switch (e.data.type) {
-    case "start":
-      startRecording(e.data.fileHandle, e.data.frameStream,
-                          e.data.trackSettings);
+    case 'start':
+      startRecording(
+        e.data.fileHandle,
+        e.data.frameStream,
+        e.data.trackSettings
+      );
       break;
-    case "stop":
+    case 'stop':
       stopRecording();
       break;
   }
